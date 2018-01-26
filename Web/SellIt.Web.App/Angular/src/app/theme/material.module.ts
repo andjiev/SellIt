7f3890152609e31421c96954744e1dfd8b150e44
andjiev <adj@mca.mk>
2018-01-27 00:50:27 +0100

    Angular migration to v5 and material theme integration

---
+++ Web/SellIt.Web.App/Angular/src/app/theme/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+} from '@angular/material';
+
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
+})
+export class MaterialModule { }
